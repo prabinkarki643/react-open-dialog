91454418975fb9c03facd7524f463ee30d2e37bb
githubprabin143 <prabinkarki643@gmail.com>
2020-10-21 19:54:25 +0530

    Updates

---
+++ precache-manifest.193f255685213c66980431165fd3c1dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f74213574a27dde07729a674f9d241",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0855a76626c8690131eb",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "b66c987258cbe54b7115",
+    "url": "./static/js/2.73152df6.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.73152df6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0855a76626c8690131eb",
+    "url": "./static/js/main.e4d39ea5.chunk.js"
+  },
+  {
+    "revision": "59bd054f3bde6b0e219b610549d13f6f",
+    "url": "./static/js/main.e4d39ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f57e2ca6b3f0c7ec6f3d",
+    "url": "./static/js/runtime-main.6ac59960.js"
+  }
+]);
