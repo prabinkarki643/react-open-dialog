8b3812a6b744a8e1fae6c275935a774ed2c5329c
githubprabin143 <prabinkarki643@gmail.com>
2020-10-22 21:42:54 +0530

    Dialog as React Component made

--- example/src/index.js
+++ example/src/index.js
@@ -3,7 +3,6 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import 'react-open-dialog/dist/index.css'
 import {RootDialog} from 'react-open-dialog'
 
 ReactDOM.render(
